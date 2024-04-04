763557dd78533e35bb0bf3b532c5d78b0d14f193
Harun29 <ibrahimovic.harun01@gmail.com>
2024-04-04 18:26:19 +0200

    picker working as expected

---
+++ roofing solution/Drawing.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+using Microsoft.Maui.Controls.Platform.Compatibility;
+
+
+namespace roofing_solution
+{
+    public class CustomDrawable : IDrawable
+    {
+        private float height;
+        private float width;
+        private float lastWidth;
+        private float panelWidth;
+        private List<double> forCount;
+
+        public CustomDrawable(double height, double width, double lastWidth, double panelWidth, List<double> forCount)
+        {
+            this.height = Convert.ToSingle(height);
+            this.width = Convert.ToSingle(width);
+            this.lastWidth = Convert.ToSingle(lastWidth);
+            this.panelWidth = Convert.ToSingle(panelWidth);
+            this.forCount = forCount;
+        }
+
+        public void Draw(ICanvas canvas, RectF dirtyRect)
+        {
+            if (canvas == null || forCount == null || forCount.Count == 0)
+                return;
+
+            canvas.StrokeColor = Colors.Black;
+            canvas.StrokeSize = 1;
+
+            width *= 40;
+            height *= 40;
+            panelWidth *= 40;
+
+            PathF path = new PathF();
+            path.MoveTo(0, height);
+            path.LineTo(width, height);
+            path.LineTo(width / 2, 0);
+            path.Close();
+            canvas.DrawPath(path);
+
+            float followingWidth = lastWidth * 40;
+            int count = 0;
+            foreach (double item in forCount)
+            {
+                canvas.StrokeDashPattern = new float[] { 2, 2 };
+                float lineHeight = Convert.ToSingle(item) * 40;
+                if (lineHeight == height && count == 0)
+                {
+                    count++;
+                    continue;
+                }
+                else if (lineHeight == height && count == 1)
+                {
+                    canvas.DrawLine(followingWidth, height, followingWidth, 0);
+                    followingWidth += panelWidth;
+                    continue;
+                }
+                canvas.DrawLine(followingWidth, height, followingWidth, height - lineHeight);
+                followingWidth += panelWidth;
+            }
+        }
+    }
+}
--- roofing solution/MainPage.xaml.cs
+++ roofing solution/MainPage.xaml.cs
@@ -8,64 +8,6 @@ using Microsoft.Maui.Controls.Platform.Compatibility;
 namespace roofing_solution
 {
 
-    public class CustomDrawable : IDrawable
-    {
-        private float height;
-        private float width;
-        private float lastWidth;
-        private float panelWidth;
-        private List<double> forCount;
-
-        public CustomDrawable(double height, double width, double lastWidth, double panelWidth, List<double> forCount)
-        {
-            this.height = Convert.ToSingle(height);
-            this.width = Convert.ToSingle(width);
-            this.lastWidth = Convert.ToSingle(lastWidth);
-            this.panelWidth = Convert.ToSingle(panelWidth);
-            this.forCount = forCount;
-        }
-
-        public void Draw(ICanvas canvas, RectF dirtyRect)
-        {
-            if (canvas == null || forCount == null || forCount.Count == 0)
-                return;
-
-            canvas.StrokeColor = Colors.Black;
-            canvas.StrokeSize = 1;
-
-            width *= 40;
-            height *= 40;
-            panelWidth *= 40;
-
-            PathF path = new PathF();
-            path.MoveTo(0, height);
-            path.LineTo(width, height);
-            path.LineTo(width/2, 0);
-            path.Close();
-            canvas.DrawPath(path);
-
-            float followingWidth = lastWidth * 40;
-            int count = 0;
-            foreach (double item in forCount)
-            {
-                canvas.StrokeDashPattern = new float[] { 2, 2 };
-                float lineHeight = Convert.ToSingle(item) * 40;
-                if(lineHeight == height && count == 0)
-                {   
-                    count++;
-                    continue;
-                }else if(lineHeight == height && count == 1)
-                {
-                    canvas.DrawLine(followingWidth, height, followingWidth, 0);
-                    followingWidth += panelWidth;
-                    continue;
-                }
-                canvas.DrawLine(followingWidth, height, followingWidth, height - lineHeight);
-                followingWidth += panelWidth;
-            }
-        }
-    }
-
     public partial class MainPage : ContentPage
     {
         // Non-nullable fields
@@ -75,6 +17,30 @@ namespace roofing_solution
         private string lossTwo = string.Empty;
         private double widthOne;
         private double widthTwo;
+        private string roofType;
+
+        private void CheckSelectedPickerItem()
+        {
+            string? selectedValue = picker.SelectedItem as string;
+
+            if (!string.IsNullOrEmpty(selectedValue))
+            {
+                roofType = selectedValue;
+                if (selectedValue == "Ravan vrh")
+                {
+                    Console.WriteLine("im here");
+                    additionalEntryField.IsVisible = true;
+                }
+                else
+                {
+                    additionalEntryField.IsVisible = false;
+                }
+            }
+            else
+            {
+                DisplayAlert("Error", "No item is selected.", "OK");
+            }
+        }
 
         // Non-nullable property
         //public List<double> Heights { get; set; } = new List<double>();
@@ -207,6 +173,8 @@ namespace roofing_solution
             InitializeComponent();
 
             this.BindingContext = this;
+
+            picker.SelectedIndexChanged += (s, e) => CheckSelectedPickerItem();
         }
 
 
